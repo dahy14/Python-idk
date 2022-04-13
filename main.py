6416ab3599cba8242056a876511122f21a6a12bf
JL Naypes <naypes.15@gmail.com>
2022-04-13 14:57:34 +0800

    v 1.3 class

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from folium import Map
+from geo import Geopoint
 
 latitude = 40.69
-longtitude = 69.69
+longitude = 69.69
 
 antipode_latitude = latitude * -1
 
-if longtitude <= 0:
-    antipode_longigtude = longtitude + 180
+if longitude <= 0:
+    antipode_longigtude = longitude + 180
 else :
-    antipode_longigtude = longtitude - 180
+    antipode_longitude = longitude - 180
 
-location = [antipode_latitude, antipode_longigtude]
+location = [antipode_latitude, antipode_longitude]
 my_map = Map(location)
-my_map.save('antipode.html')
+# my_map.save('antipode.html')
 
-print ("Antipode Latitude: " + antipode_latitude)
-print ("Antipode Longtitude: " + antipode_longigtude)
+my_point = Geopoint(latitude, longitude)
+print(my_point.closest_parallel())
 
-print(my_map)
+print ("Antipode Latitude: " + str(antipode_latitude))
+print ("Antipode Longitude: " + str(antipode_longitude))
+
 
 # You have stolen my heart oh yeah
