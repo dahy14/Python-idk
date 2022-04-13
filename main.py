f80dc3d70c11e42e389dce4004068b8a7ef6dec0
JL Naypes <naypes.15@gmail.com>
2022-04-13 10:55:22 +0800

    v 1.2

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-latitude = float("40.09")
-longtitude = float("72.64")
+from folium import Map
 
-antipode_latitude = latitude.__mul__(int("-1"))
+latitude = 40.69
+longtitude = 69.69
 
-if longtitude < 0:
-    antipode_longigtude = longtitude.__add__(float("180"))
+antipode_latitude = latitude * -1
+
+if longtitude <= 0:
+    antipode_longigtude = longtitude + 180
 else :
-    antipode_longigtude = longtitude.__sub__(float("180"))
+    antipode_longigtude = longtitude - 180
 
+location = [antipode_latitude, antipode_longigtude]
+my_map = Map(location)
+my_map.save('antipode.html')
 
-print ("Latitude: " + str(latitude))
-print ("Longtitude: " + str(longtitude))
-print ("Antipode Latitude: " + str(antipode_latitude))
-print ("Antipode Longtitude: " + str(antipode_longigtude))
+print ("Antipode Latitude: " + antipode_latitude)
+print ("Antipode Longtitude: " + antipode_longigtude)
+
+print(my_map)
 
 # You have stolen my heart oh yeah
